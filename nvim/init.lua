3749377a842e11d78a84c3a73625b1a7a7a8ba0f
Cham <450139220@qq.com>
2025-12-10 11:14:28 +0800

    chore: modify theme to catppuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,26 @@
 require("config.options")
 require("config.lazy")
 
+-- Built-ins
 -- vim.cmd("colorscheme slate")
 -- vim.cmd("colorscheme habamax")
+-- vim.cmd("colorscheme lunaperche")
+
+-- Plugins
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme nordic")
-vim.cmd("colorscheme lunaperche")
+-- vim.cmd("colorscheme github_dark_default")
+vim.cmd("colorscheme catppuccin")
 
 -- Set comment to italic and be default color
 local function mod_hl(group, attrs)
-  local current_hl = vim.api.nvim_get_hl_by_name(group, true)
-  if current_hl then
-    local new_hl = vim.tbl_deep_extend("force", current_hl, attrs)
-    vim.api.nvim_set_hl(0, group, new_hl)
-  end
+    local current_hl = vim.api.nvim_get_hl_by_name(group, true)
+    if current_hl then
+        local new_hl = vim.tbl_deep_extend("force", current_hl, attrs)
+        vim.api.nvim_set_hl(0, group, new_hl)
+    end
 end
-mod_hl("Comment", { italic = true })
+-- mod_hl("Comment", { italic = true })
 
 -- Indent mini color
 vim.cmd.highlight("IndentLine guifg=#333333")
